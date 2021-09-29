dbb916db69b21b524119412c49ec5ab9971ae306
jackyuan256 <jackyuan256@gmail.com>
2021-09-29 22:37:18 +0800

    去除多余符号

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const router = createRouter({
     history: history,
     routes: [
         {path: '/', component: Yuan},
-        {path: '/xxx', component: Yuan},
+        {path: '/xxx', component: Yuan}
     ]
 })
 const app = createApp(App)
