9cccf64ccc9bcbbd42bae775a0b029dd722f959d
jackyuan256 <jackyuan256@gmail.com>
2021-10-01 19:53:35 +0800

    添加其他路由

---
+++ src/components/Xxx.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h4>Xxx</h4>
+</template>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import App from './App.vue'
 import {createRouter, createWebHashHistory} from 'vue-router'
 
 import Yuan from './components/Yuan.vue'
+import Xxx from './components/Xxx.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
     history: history,
     routes: [
         {path: '/', component: Yuan},
-        {path: '/xxx', component: Yuan}
+        {path: '/xxx', component: Xxx}
     ]
 })
 const app = createApp(App)
