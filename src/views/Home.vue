04796de572a2d6fffa17ec6275fa7f8e722dfdc4
jackyuan256 <jackyuan256@gmail.com>
2021-10-08 21:57:26 +0800

    Home 页面布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Topnav/>
+        <Topnav />
         <div class="banner">
             <h1>Zao UI</h1>
             <h2>一个基础的 UI 框架</h2>
@@ -17,5 +17,25 @@ import Topnav from '../components/Topnav.vue'
 </script>
 
 <style lang="scss" scoped>
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
