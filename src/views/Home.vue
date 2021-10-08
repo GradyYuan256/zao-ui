364df2ce03eac68e70776f47e6d87351fef00d5c
jackyuan256 <jackyuan256@gmail.com>
2021-10-08 22:10:17 +0800

    初始化 aside 的 html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
             <h1>Zao UI</h1>
             <h2>一个基础的 UI 框架</h2>
             <p class="actions">
-                <a href="#">Github</a>
-                <a href="#">开始</a>
+                <a href="https://github.com/jackyuan256/zao-ui" target="_blank">Github</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
