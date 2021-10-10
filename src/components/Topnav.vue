6bfb789ec377e41b252e509364f67fb95dd25e01
jackyuan256 <jackyuan256@gmail.com>
2021-10-10 21:27:06 +0800

    添加 toggleAside 以及显示隐藏

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -14,12 +14,12 @@ const toggleMenu = () => {
 
 <template>
     <div class="topnav">
-        <div class="logo" @click="toggleMenu">LOGO</div>
+        <div class="logo">LOGO</div>
         <ul class="menu">
             <li>菜单1</li>
             <li>菜单2</li>
         </ul>
-        <span class="toggleAside"></span>
+        <span class="toggleAside" @click="toggleMenu"></span>
     </div>
 </template>
 
@@ -52,6 +52,7 @@ const toggleMenu = () => {
             left: 16px;
             top: 50%;
             transform: translateY(-50%);
+            display: none;
         }
         @media (max-width:500px) {
             > .menu {
@@ -59,6 +60,9 @@ const toggleMenu = () => {
             }
             > .logo {
                 margin: 0 auto;
+            }
+            > .toggleAside {
+                display: inline-block;
             }
         }
     }
