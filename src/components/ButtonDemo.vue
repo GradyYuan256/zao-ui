4bb33bc027b0ec78cb8eaaa0863c5a4c4b6463cd
jackyuan256 <jackyuan256@gmail.com>
2021-10-20 21:24:58 +0800

    添加 click

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -11,7 +11,7 @@ const onClick = () => {
     <div>Button 示例</div>
     <h1>示例1</h1>
     <div>
-        <Button>你好</Button>
+        <Button @click="onClick">你好</Button>
         <Button theme="button">你好</Button>
         <Button theme="link">你好</Button>
         <Button theme="text">你好</Button>
