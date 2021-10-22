0e9bba0b4d86484ace2c76dbfa6f4cb73dc0ce89
jackyuan256 <jackyuan256@gmail.com>
2021-10-22 22:42:56 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,113 +1,13 @@
 <script setup lang="ts">
+import Dialog from '../lib/Dialog.vue'
 
 </script>
 
 <template>
-    <div>
-        Dialog 文档
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-    </div>
+    <div>Dialog 示例</div>
+    <h1>示例1</h1>
+    <Dialog></Dialog>
 </template>
 
 <style lang="scss" scoped>
-    
 </style>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Button from '../lib/Button.vue'
+
+</script>
+
+<template>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wrapper">
+        <header>标题</header>
+        <main>
+            <p>第一行字</p>
+            <p>第二行字</p>
+        </main>
+        <footer>
+            <Button>OK</Button>
+            <Button>Cancel</Button>
+        </footer>
+    </div>
+</template>
